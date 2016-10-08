2eadf1ee8ed2d3f8fb1c45b8b66b0178ed9b7774
swb <swbsin@163.com>
2016-10-08 14:54:42 +0800

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,42 @@
+sudo su
+sudo apt-get update 
+sudo apt-get install apt-transport-https ca-certificates -y
+sudo apt-get install git -y
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo bash -c "echo  deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list"
+
+sudo apt-get update
+sudo apt-get purge lxc-docker
+
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
+
+sudo apt-get update
+sudo apt-get install docker-engine -y
+sudo service docker start
+sudo docker run hello-world
+
+curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+docker pull hyperledger/fabric-peer:latest
+docker pull hyperledger/fabric-membersrvc:latest
+
+curl -O https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.7.1.linux-amd64.tar.gz
+mkdir -p ~/go; echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin" >> ~/.bashrc
+source ~/.bashrc
+
+
+mkdir -p $GOPATH/src/github.com/chaincode_example02/
+cd $GOPATH/src/github.com/chaincode_example02
+curl GET https://raw.githubusercontent.com/hyperledger/fabric/master/examples/chaincode/go/chaincode_example02/chaincode_example02.go > chaincode_example02.go
+
+mkdir -p $GOPATH/src/github.com/hyperledger
+cd $GOPATH/src/github.com/hyperledger
+git clone http://gerrit.hyperledger.org/r/fabric
+
+cd $GOPATH/src/github.com/chaincode_example02
+go build
